4fedb2e67a0fd07c741f229b4d235a96c53937f2
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2026-03-02 09:49:02 -0700

    fix(label): improve range label formating

--- src/e3sm_quickview/module/serve/utils.js
+++ src/e3sm_quickview/module/serve/utils.js
@@ -6,6 +6,21 @@ window.trame.utils.quickview = {
     if (useLog && value > 0) {
       return `10^(${Math.log10(value).toFixed(1)})`;
     }
+    const nSignDigit = Math.log10(Math.abs(value));
+    if (Math.abs(nSignDigit) < 6 || value === 0) {
+      if (nSignDigit > 0 && nSignDigit < 3) {
+        return new Intl.NumberFormat("US", { maximumFractionDigits: 2 }).format(
+          value,
+        );
+      }
+      if (nSignDigit > 3) {
+        return new Intl.NumberFormat("US", { maximumFractionDigits: 0 }).format(
+          value,
+        );
+      }
+
+      return new Intl.NumberFormat().format(value);
+    }
     return value.toExponential(1);
   },
   cols(size) {
